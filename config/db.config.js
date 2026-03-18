52795d69aea304e8a362964a2923d5fbf0930474
saad <amzouksaad7@gmail.com>
2026-03-18 04:21:46 +0000

    fix db config

--- config/db.config.js
+++ config/db.config.js
@@ -6,8 +6,9 @@ const pool = mysql.createPool({
   user:     process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port:     process.env.DB_PORT || 3306,
   waitForConnections: true,
-  connectionLimit:    10,
+  connectionLimit: 10,
 });
 
 module.exports = pool.promise();
